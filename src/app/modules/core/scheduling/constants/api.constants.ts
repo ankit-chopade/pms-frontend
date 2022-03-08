46af738ace5ea1f88b4e4ef53a6c3ee5c6584443
Ankit Chopade <ankit.chopade@outlook.com>
2022-03-08 17:47:37 +0530

    added rest end points

--- src/app/modules/core/scheduling/constants/api.constants.ts
+++ src/app/modules/core/scheduling/constants/api.constants.ts
@@ -1,12 +1,12 @@
 export const APIConst={
- PHYSICIAN:"getPhysician",
- PATIENT:"getPatient",
- SAVE_APPOINTMENT:'savePatientAppointment',
- PHYSICIAN_APPOINTMENTS:"getPhysicianAppointments",
- EDIT_APPOINTMENT:"editPatientAppointment",
- DELETE_PHYSICIAN_APPOINTMENT:"deletePhysicianAppointment",
- SAVE_EDIT_HISTORY:'edithistory',
- GET_EDIT_HISTORY:'edithistorypatient',
- PATIENT_APPOINTMENTS:"getPatientAppointments",
+ PHYSICIAN:"/schedule/getPhysician",
+ PATIENT:"/schedule/getPatient",
+ SAVE_APPOINTMENT:'/schedule/savePatientAppointment',
+ PHYSICIAN_APPOINTMENTS:"/schedule/getPhysicianAppointments",
+ EDIT_APPOINTMENT:"/schedule/editPatientAppointment",
+ DELETE_PHYSICIAN_APPOINTMENT:"/schedule/deletePhysicianAppointment",
+ SAVE_EDIT_HISTORY:'/schedule/edithistory',
+ GET_EDIT_HISTORY:'/schedule/edithistorypatient',
+ PATIENT_APPOINTMENTS:"/schedule/getPatientAppointments",
 
 }
